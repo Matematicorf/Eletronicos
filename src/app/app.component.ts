50e5a8bfee89752afc18cb07d92ffc1052cb8a04
Rafaelshelby19 <rafaelalexalvesmorass@gmail.com>
2026-09-18 13:41:10 -0300

    atualizações do codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,37 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   template: `
-    <!-- Cabeçalho fixo -->
-  <header style="background-color: #f4f4f4; padding: 10px; text-align: center;">
-  <h1 style="font-family: Arial, sans-serif !important; margin: 0;">Alves eletrônicos</h1>
-</header>
+<!-- Cabeçalho fixo -->
+   <!-- Cabeçalho fixo -->
+    <header style="position: fixed; top: 0; left: 0; width: 100%; height: 80px; background-color:  #f1f5f9; display: flex; align-items: center; justify-content: center; z-index: 1000; padding: 0 20px;">
+      
+      <img
+        src="/assets/logo-alves.png"
+        alt="Logo Alves Eletrônicos"
+        style="
+          /* --- TAMANHO --- */
+          height: 280px;             /* Ajuste a altura exata aqui */
+          width: auto;              /* Deixe auto para não distorcer a imagem */
+          object-fit: contain;
+          transform: translate(-500px, 1px); /* X: -500px (esquerda) | Y: 15px (baixo) */
+          /* Opcional para ajuste milimétrico de posição */
+          position: relative;
+          top: 0px;                 /* Ajuste fino vertical */
+          left: 0px;                /* Ajuste fino horizontal */
+          
+          
+        "
+      >
+  
 
-    <main>
+    </header>
+
+    <main style="margin-top: 50px; padding: 0px; background-color: #f2f2f2;">
       <router-outlet></router-outlet>
     </main>
   `
 })
+
 export class AppComponent {
-  title = 'Alves eletrônicos';
+  title = 'Alves';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component'; // Certifique-se que o caminho é exatamente este
-
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
